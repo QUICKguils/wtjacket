81ff00b1563a8683e0a017cfeabf8153054122b4
PolyGON3 <37215227+PolyGON3@users.noreply.github.com>
2023-10-07 19:07:14 +0200

    added mass convergence study
    in new docked figure
    - total mass plot
    - mass ratio convergence plot

--- wtjacket/utils/fem_convergence.m
+++ wtjacket/utils/fem_convergence.m
@@ -6,6 +6,8 @@ function fem_convergence
 	% - implement other ways to analyze the convergence, e.g. MAC w.r.t. the
 	%   NX modes ?
 
+    % TODO mass convergence analysis
+
 	% Directory where the present file lies.
 	file_dir = fileparts(mfilename("fullpath"));
 	% Results file.
@@ -20,20 +22,43 @@ function fem_convergence
     
 
 	% Desired sdiv sequence.
-	sdiv_serie = 1:9;
+	sdiv_serie = 1:4;
     
 	freqs_serie = zeros(numel(sdiv_serie), SOL.nbMode);
-    modes_serie = zeros(numel(sdiv_serie), BS.nbDOF, SOL.nbMode);
+    modes_serie = zeros(numel(sdiv_serie), BS.nbDOF, SOL.nbMode);    
+    masses_serie = zeros(numel(sdiv_serie), 2);
     
 	% Gather the frequencies for the desired subdivisions.
 	for i = 1:numel(sdiv_serie)
 		modeling(sdiv_serie(i), '');
 		SOL = load("res\modeling_sol.mat");
-		freqs_serie(i, :) = SOL.freqs;
+        BS = load(fullfile(res_file, "bare_struct.mat"));
+
+		masses_serie(i, 1) = SOL.mass_rbm;
+        masses_serie(i, 2) = SOL.mass_rbm/BS.mass;
+        
+        freqs_serie(i, :) = SOL.freqs;
+        
         for d = 1:BS.nbDOF
             modes_serie(i, d, :) = SOL.modes(d,:);
 		end
     end
+    
+    % Mass convergence
+    figure("WindowStyle", "docked");
+    subplot(1,2,1);
+    plot(sdiv_serie, masses_serie(:, 1));
+	title("RBM Total Mass convergence");
+	xlabel("Number of sub-elements");
+	ylabel("Total mass from RBM (kg)");
+	grid;
+    
+    subplot(1,2,2);
+    plot(sdiv_serie, masses_serie(:, 2));
+	title("RBM Total Mass - Theoretical Mass Ratio convergence"); % TODO better title ?
+	xlabel("Number of sub-elements");
+	ylabel("Mass ratio [-]");
+	grid;
     
 	% Compute the relative differences between sdiv steps.
 	reldiffs = abs(diff(freqs_serie)./freqs_serie(2:end, :));
@@ -44,13 +69,13 @@ function fem_convergence
 
     for i = 1:numel(sdiv_serie)-1
         mode_diff(i, :, :) = abs(modes_serie(i+1, :, :) -  modes_serie(i, :, :));
+        
         for m = 1:SOL.nbMode
             norm_mode_diff(i,m) = norm(mode_diff(i, :, m));
         end
     end
     
     % MAC
-    
     % this section computes the MAC for each successive mode
     MAC_arr = zeros(numel(sdiv_serie)-1, SOL.nbMode, SOL.nbMode);
     
@@ -120,5 +145,3 @@ end
         end
     end
  end
- 
-    
