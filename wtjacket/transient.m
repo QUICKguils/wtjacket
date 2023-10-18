50eaf1f6a77c61df9d093562f9eef0fa9f81441c
QUICKguils <guilainsjc@gmail.com>
2023-10-18 16:06:47 +0200

    Improve modal displacement performance
    The functional programming style of defining mathematical functions was
    discarded. The code now only returns array, instead of function handles.

--- wtjacket/transient.m
+++ wtjacket/transient.m
@@ -9,24 +9,22 @@ if ModelingSolution.nMode < 8
 end
 
 [eps, ~] = set_damping_matrix(Constant.DAMPING_RATIO, ...
-                              Constant.DAMPING_RATIO, ...
-                              ModelingSolution.frequencyRad, ...
-                              ModelingGlobalMatrix.K, ...
-                              ModelingGlobalMatrix.M);
+	Constant.DAMPING_RATIO, ...
+	ModelingSolution.frequencyRad, ...
+	ModelingGlobalMatrix.K, ...
+	ModelingGlobalMatrix.M);
 
-% TODO: probably put this is bare_structure.m
+% TODO: probably put this constant in bare_structure.m or load_constant.m
 IMPACT_NODE_LABEL = 18;
-p = create_loading(Constant, SubdivisedStructure, IMPACT_NODE_LABEL);
+TIME_SAMPLE = 0:0.02:10;
+load = create_load(Constant, SubdivisedStructure, IMPACT_NODE_LABEL, TIME_SAMPLE);
 
 % TODO: register ICs in Constants.
-ModalSuperposition = modal_superposition(ModelingSolution, ModelingGlobalMatrix, eps, p, [0, 0]);
-
-q = mode_displacement(8, ModalSuperposition.nu, ModelingSolution.mode);
-
-tSample = 0:0.02:10;
-dof = SubdivisedStructure.nodeList{18}.dof(1);  % excitation node, x comp.
-qt = q(tSample);
-plot(tSample, qt(dof, :));
+ModalSuperposition = modal_superposition(ModelingSolution, ModelingGlobalMatrix, eps, load, TIME_SAMPLE, [0, 0]);
+q = mode_displacement(ModelingSolution.nMode, ModalSuperposition.nu, ModelingSolution.mode);
+plot(TIME_SAMPLE ,q(SubdivisedStructure.nodeList{18}.dof(1), :));
+hold on;
+plot(TIME_SAMPLE ,q(SubdivisedStructure.nodeList{18}.dof(2), :));
 end
 
 function [eps, C] = set_damping_matrix(eps1, eps2, w0, K, M)
@@ -49,30 +47,24 @@ C   = a*K + b*M;
 end
 
 
-function p = create_loading(C, SS, impactNodeLabel)
+function load = create_load(C, SS, impactNodeLabel, t)
+% TODO: make it more general.
+% Allow user to enter multiple excited node, force directions and amplitudes.
 
 % Assume an excitation force F = A*sin(w*t).
-A = 0.5 * C.TAIL_MASS*C.TAIL_SPEED*C.MOMENTUM_TRANSFER / C.IMPACT_DURATION;
-w = C.LOAD_FREQUENCY * 2*pi;
-F  = @(t) A*sin(w*t);
-Fx = @(t)  F(t) * sind(45);
-Fy = @(t) -F(t) * sind(45);
+% TODO: make sure of this chunck of code.
+amplitude = 0.5 * C.TAIL_MASS*C.TAIL_SPEED*C.MOMENTUM_TRANSFER / C.IMPACT_DURATION;
+frequencyRad = C.LOAD_FREQUENCY * 2*pi;
+force  = @(t) amplitude*sin(frequencyRad*t);
+forceX = @(t)  force(t) * sind(45);
+forceY = @(t) -force(t) * sind(45);
 
-	% inner function of the closure: isolate parameter `t`.
-	function p_at_t = load_at_t(t)
-		% In: (double) time instant [s].
-		% Out: (1 x nDof double) -- loads for all Dofs at ´t´ [N].
-
-		% PERF: maybe put prealloc outside the inner func
-		p_at_t = zeros(SS.nDof, numel(t));
-		p_at_t(SS.nodeList{impactNodeLabel}.dof(1), :) = Fx(t);
-		p_at_t(SS.nodeList{impactNodeLabel}.dof(2), :) = Fy(t);
-	end
-
-	p = @(t) load_at_t(t);
+load = zeros(SS.nDof, numel(t));
+load(SS.nodeList{impactNodeLabel}.dof(1), :) = forceX(t);
+load(SS.nodeList{impactNodeLabel}.dof(2), :) = forceY(t);
 end
 
-function ModalSuperposition = modal_superposition(SOL, KM, eps, p, initialConditions)
+function ModalSuperposition = modal_superposition(SOL, KM, eps, load, t, initialConditions)
 % return the nu and the phi.
 
 % TODO: determine these IC.
@@ -80,26 +72,24 @@ function ModalSuperposition = modal_superposition(SOL, KM, eps, p, initialCondit
 A = initialConditions(1);
 B = initialConditions(2);
 
-mu  = zeros(1, SOL.nMode);
-phi = cell(1, SOL.nMode);
-wd  = zeros(1, SOL.nMode);
-h   = cell(1, SOL.nMode);
-nu  = cell(1, SOL.nMode);
+mu  = zeros(SOL.nMode, 1);
+phi = zeros(SOL.nMode, length(t));
+wd  = zeros(SOL.nMode, 1);
+h   = zeros(SOL.nMode, length(t));
+nu  = zeros(SOL.nMode, length(t));
 
-% PERF: see if nested function handles are quick enough.
 for r = 1:SOL.nMode
-	% Generalized masses.
-	% TODO: verify if relevant to do that.
-	% modes are probably already mass normalized.
+
 	mu(r) = SOL.mode(:, r)'*KM.M_free*SOL.mode(:, r);
 	% TODO: check mult dims when p(t) is done.
-	phi{r} = @(t) SOL.mode(:, r)' * p(t)/mu(r);
+	phi(r, :) = SOL.mode(:, r)' * load / mu(r);
 
 	wd(r) = sqrt(1-eps(r)^2)*SOL.frequencyRad(r);
-	h{r} = @(t) 1/wd(r) .* exp(-eps(r)*wd(r)*t) .* sin(wd(r)*t);
-	nu_transient = @(t) exp(-eps(r)*wd(r)*t) .* (A*cos(wd(r)*t) + B*sin(wd(r)*t));
-	nu_permanent = @(ts) arrayfun(@(t) integral(@(s) phi{r}(s).*h{r}(t-s), 0, t), ts);
-	nu{r} = @(t) nu_transient(t) + nu_permanent(t);
+	h(r, :) = 1/wd(r) .* exp(-eps(r)*wd(r)*t) .* sin(wd(r)*t);
+	nu_transient = exp(-eps(r)*wd(r)*t) .* (A*cos(wd(r)*t) + B*sin(wd(r)*t));
+	nu_permanent = conv(phi(r, :), h(r, :), 'same');
+	% nu_permanent = arrayfun(@(t) integral(@(s) phi{r}(s).*h{r}(t-s), 0, t), tSample);
+	nu(r, :) = nu_transient + nu_permanent;
 end
 
 ModalSuperposition.phi = phi;
@@ -109,9 +99,9 @@ end
 
 function q = mode_displacement(k, nu, modes)
 % Compute q with the mode displacement method.
-q = @(t) 0;
+q = zeros(size(modes, 1), 1);
 for r = 1:k
-	q = @(t) q(t) + modes(:, r) * nu{r}(t);
+	q = q + modes(:, r) * nu(r, :);
 end
 end
 
